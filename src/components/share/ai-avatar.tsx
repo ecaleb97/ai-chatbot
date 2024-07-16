840ec50603b411380e2fd98e5b8f72dccdd7741d
ecaleb97 <valley_rob.0c@icloud.com>
2024-07-16 18:50:13 +0200

    add avatar components

--- src/components/share/ai-avatar.tsx
+++ src/components/share/ai-avatar.tsx
@@ -3,8 +3,13 @@ import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 export function AIAvatar() {
 	return (
 		<Avatar>
-			<AvatarImage src="/logo-ai-project.svg" className="rounded-full object-cover size-7" />
-			<AvatarFallback className="rounded-full object-cover size-7">AI</AvatarFallback>
+			<AvatarImage
+				src="/logo-ai-project.svg"
+				className="rounded-full object-cover size-7"
+			/>
+			<AvatarFallback className="rounded-full object-cover size-7">
+				AI
+			</AvatarFallback>
 		</Avatar>
 	);
 }
--- src/components/share/user-avatar.tsx
+++ src/components/share/user-avatar.tsx
@@ -6,7 +6,10 @@ export function UserAvatar() {
 
 	return (
 		<Avatar>
-			<AvatarImage src={user?.imageUrl} className=" size-7 rounded-full object-cover" />
+			<AvatarImage
+				src={user?.imageUrl}
+				className=" size-7 rounded-full object-cover"
+			/>
 			<AvatarFallback className="size-7 rounded-full object-cover">
 				{user?.firstName?.charAt(0)}
 				{user?.lastName?.charAt(0)}
