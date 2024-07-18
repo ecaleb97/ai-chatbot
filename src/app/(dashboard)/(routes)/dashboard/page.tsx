d7c47afdab7a878c17196f7a5972623a0a171257
ecaleb97 <valley_rob.0c@icloud.com>
2024-07-18 22:04:24 +0200

    remove unused code

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -2,53 +2,9 @@
 
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
-import {
-	ArrowRight,
-	Code,
-	ImageIcon,
-	MessageSquare,
-	Music,
-	VideoIcon,
-} from "lucide-react";
+import { ArrowRight } from "lucide-react";
 import { useRouter } from "next/navigation";
-
-const tools = [
-	{
-		label: "Conversation",
-		icon: MessageSquare,
-		color: "text-violet-500",
-		bgColor: "bg-violet-500/10",
-		href: "/conversation",
-	},
-	{
-		label: "Image Generation",
-		icon: ImageIcon,
-		color: "text-pink-700",
-		bgColor: "bg-pink-700/10",
-		href: "/image-generation",
-	},
-	{
-		label: "Video Generation",
-		icon: VideoIcon,
-		color: "text-orange-500",
-		bgColor: "bg-orange-500/10",
-		href: "/video-generation",
-	},
-	{
-		label: "Music Generation",
-		icon: Music,
-		color: "text-emerald-500",
-		bgColor: "bg-emerald-500/10",
-		href: "/music-generation",
-	},
-	{
-		label: "Code Generation",
-		icon: Code,
-		color: "text-blue-500",
-		bgColor: "bg-blue-500/10",
-		href: "/code-generation",
-	},
-];
+import { tools } from "@/lib/constants";
 
 export default function DashboardPage() {
 	const router = useRouter();
