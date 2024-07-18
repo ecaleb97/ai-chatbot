480c9c2c2b0d799af97dd2f70d83bcb1037eb7ea
ecaleb97 <valley_rob.0c@icloud.com>
2024-07-18 22:05:22 +0200

    add subscription schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,9 @@
+import { createId } from "@paralleldrive/cuid2";
 import { sql } from "drizzle-orm";
 import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
 
 export const userApiLimit = sqliteTable("user_api_limit", {
-	id: text("id").primaryKey(),
+	id: text("id").primaryKey().default(createId()),
 	userId: text("user_id").unique(),
 	count: integer("count").default(0),
 	createdAt: text("created_at")
@@ -12,4 +13,15 @@ export const userApiLimit = sqliteTable("user_api_limit", {
 		.notNull()
 		.default(sql`(current_timestamp)`)
 		.$onUpdate(() => sql`(current_timestamp)`),
+});
+
+export const userSubscription = sqliteTable("user_subscription", {
+	id: text("id").primaryKey().default(createId()),
+	userId: text("user_id").unique(),
+	stripeCustomerId: text("stripe_customer_id").unique(),
+	stripeSubscriptionId: text("stripe_subscription_id").unique(),
+	stripePriceId: text("stripe_price_id").unique(),
+	stripeCurrentPeriodEnd: integer("stripe_current_period_end", {
+		mode: "timestamp_ms",
+	}),
 });
